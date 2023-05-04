d907ce907aa599e17c594ce696d5da5fdd38a1a9
Berriondo <juan.gomezio@amigo.edu.co>
2023-05-04 10:19:50 -0500

    Add Cors

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import employeesRoutes from './routes/employes.routes.js'
 import indexRoutes from './routes/index.routes.js'
 import rrhhRoutes from "./routes/rrhh.routes.js";
 import {PORT} from "./config.js"
+import cors from 'cors'
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
+
 app.use(indexRoutes)
 app.use('/api',employeesRoutes)
 app.use('/api',rrhhRoutes)
